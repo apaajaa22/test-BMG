f26f638f090589c1d2d7df488d1e45ab70407d86
Rahadian Reza Rizaldy <59022153+apaajaa22@users.noreply.github.com>
2021-10-13 11:47:50 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
-//bisa di bagi 3  &  5 & 15
-
-const solutions =  (s) => {
+const solutions = (s) => {
   let arr = []
-  for (let i= 1; i <= s; i++ ){
-    if (i%3===0 && i%5===0){
-      arr.push(i,'Frontend Backend')
-    }
-    else if (i%3===0){
-      arr.push(i,'Frontend')
-    }else if (i%5===0){
-      arr.push(i,'Backend')
-    }else {
+  for (let i = 1; i <= s; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      arr.push("Frontend Backend")
+    } else if (i % 3 === 0) {
+      arr.push("Frontend")
+    } else if (i % 5 === 0) {
+      arr.push("Backend")
+    } else {
       arr.push(i)
     }
   }
-  return arr
+  return arr.toString()
 }
-
 console.log(solutions(50))
-// solutions(50)
